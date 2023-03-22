880262ba8aaa5c98f98d3b81301316328d9f2c10
unknown <manurunghutagurgur168@gmail.com>
2023-03-22 21:33:44 +0700

    first commit

---
+++ resources/views/book.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 10 Crud operation with ajax</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" />
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/select/1.6.2/css/select.bootstrap5.min.css" rel="stylesheet">
+   
+</head>
+<body>
+    
+<div class="container">
+    <h1>Contoh Laravel 10 Crud with Ajax</h1>
+    <br>
+    <br>
+    <a class="btn btn-primary" href="javascript:void(0)" id="createNewBook"> Create New</a>
+    <br>
+    <br>
+    <table class="table-info data-table table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th width="300px">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+   
+<div class="modal fade" id="ajaxModel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="modelHeading"></h4>
+                
+            </div>
+            <div class="modal-body">
+                <form id="bookForm" name="bookForm" class="form-horizontal">
+                   <input type="hidden" name="book_id" id="book_id">
+                    <div class="form-group">
+                        <label for="name" class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-12">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" value="" maxlength="50" required="">
+                        </div>
+                    </div>
+     
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Details</label>
+                        <div class="col-sm-12">
+                            <textarea id="author" name="author" required="" placeholder="Enter Author" class="form-control"></textarea>
+                        </div>
+                    </div>
+      
+                    <div class="col-sm-offset-2 col-sm-10">
+                    <div class="modal-footer">
+                    
+                    <button type="button" class="btn btn-primary" id="saveBtn" value="create">Save changes</button>
+                   
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>  
+<script type="text/javascript">
+  $(function () {
+      $.ajaxSetup({
+          headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+    });
+    var table = $('.data-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('books.index') }}",
+        columns: [
+            {data: 'id', name: 'id'},
+            {data: 'title', name: 'title'},
+            {data: 'author', name: 'author'},
+            {data: 'action', name: 'action', orderable: false, searchable: false},
+        ]
+    });
+    $('#createNewBook').click(function () {
+        $('#saveBtn').val("create-book");
+        $('#book_id').val('');
+        $('#bookForm').trigger("reset");
+        $('#modelHeading').html("Create New Book");
+        $('#ajaxModel').modal('show');
+    });
+    $('body').on('click', '.editBook', function () {
+      var book_id = $(this).data('id');
+      $.get("{{ route('books.index') }}" +'/' + book_id +'/edit', function (data) {
+          $('#modelHeading').html("Edit Book");
+          $('#saveBtn').val("edit-book");
+          $('#ajaxModel').modal('show');
+          $('#book_id').val(data.id);
+          $('#title').val(data.title);
+          $('#author').val(data.author);
+      })
+   });
+    $('#saveBtn').click(function (e) {
+        e.preventDefault();
+        $(this).html('Save');
+    
+        $.ajax({
+          data: $('#bookForm').serialize(),
+          url: "{{ route('books.store') }}",
+          type: "POST",
+          dataType: 'json',
+          success: function (data) {
+     
+              $('#bookForm').trigger("reset");
+              $('#ajaxModel').modal('hide');
+              table.draw();
+         
+          },
+          error: function (data) {
+              console.log('Error:', data);
+              $('#saveBtn').html('Save Changes');
+          }
+      });
+    });
+    
+    $('body').on('click', '.deleteBook', function () {
+     
+        var book_id = $(this).data("id");
+        confirm("Are You sure want to delete?");
+      
+        $.ajax({
+            type: "DELETE",
+            url: "{{ route('books.store') }}"+'/'+book_id,
+            
+            success: function (data) {
+                table.draw();
+            },
+            error: function (data) {
+                console.log('Error:', data);
+            }
+        });
+    });
+     
+  });
+</script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html>
